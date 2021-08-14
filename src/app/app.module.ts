3cb31cf70c97ad9e8443531a8326f0ba5b9d363d
alexey_horbunov <algoritm211@gmail.com>
2021-08-14 19:00:00 +0300

    Add routing and form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AppReducer} from "./reducers";
 import { PostListComponent } from './post-list/post-list.component';
+import { AddPostComponent } from './add-post/add-post.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent
+    PostListComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     StoreModule.forRoot(AppReducer, {}),
     EffectsModule.forRoot([])
   ],
