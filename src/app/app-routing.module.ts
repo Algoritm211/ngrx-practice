c3d46119728ecf7d144bb737217de6daae8bbb14
alexey_horbunov <algoritm211@gmail.com>
2021-08-15 19:00:00 +0300

    Add routing and form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AddPostComponent} from "./add-post/add-post.component";
+import {PostListComponent} from "./post-list/post-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostListComponent },
+  { path: 'add-post', component: AddPostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
